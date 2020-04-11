64e83d915d2023b14ee3d78f4f691e5b69698825
Mateusz <mateuszharazin@wp.pl>
2020-04-11 17:15:04 +0200

    Add source file and reding from file
    Signed-off-by: Mateusz <mateuszharazin@wp.pl>

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import pandas as pd
 
-print("HelloWorld!")
+data = pd.read_csv("Resources/wdbc.csv", sep=',', header=None).transpose().values
+
+kNN = [1, 5, 10]
+metricType = ['mangattan', 'euclidean', 'minkowski']
+
+values = []
+
+
+# data[2][1] --> 3cia kolumna 2gi wiersz
+print(data[2][1])
+
