8efa55f891e59fa7005b30210b6f82942d055b65
cezaro <bejzyl@gmail.com>
2020-05-25 03:28:59 +0200

    Working kNN

--- instance.py
+++ instance.py
@@ -2,11 +2,17 @@ class Instance:
     id: int
     featureValues: list
     cancerType: int
+    cancerClass: str
 
     def __init__(self, id, featureValues, cancerType):
         self.id = id
         self.featureValues = featureValues
         self.cancerType = cancerType
+
+        if int(cancerType) == 2:
+            self.cancerClass = 'B'
+        else:
+            self.cancerClass = 'M'
 
     def getId(self):
         return self.id
@@ -14,5 +20,13 @@ class Instance:
     def getFeatureValues(self):
         return self.featureValues
 
+    def getFeature(self, feature):
+        value = self.getFeatureValues()[feature]
+
+        return value if value != '?' else 0
+
     def getCancerType(self):
         return self.cancerType
+
+    def getCancerClass(self):
+        return self.cancerClass
--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from scipy.stats import ks_2samp
 from ksType import ksType
 from instance import Instance
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import random
 
 
 def main():
     fileName = 'Resources/breast-cancer-wisconsin.data'
+    # fileName = 'Resources/wdbc.data'
+
     # kNN = [1, 5, 10]
-    # metricType = ['mangattan', 'euclidean', 'minkowski']
+    # metricType = ['manhattan', 'euclidean', 'minkowski']
+
     quantityOfFeatures = 9
     instances = loadDataFromFile(fileName)
     ranking = kolmogorovTest(instances, quantityOfFeatures)
+    featuresIds = [x.getParamID() for x in ranking]
 
-    for feature in range(0, ranking.__len__()):
-        print(ranking[feature].getParamID(), '\t', ranking[feature].getPValue(), '\t', ranking[feature].getStatistic())
+    # for feature in range(0, ranking.__len__()):
+    #     print(ranking[feature].getParamID() + 1, '\t', ranking[feature].getPValue(), '\t', ranking[feature].getStatistic())
+
+    # 2-krotna walidacja krzyżowa
+    teachingData, testData = crossValidation(instances)
+    # print(instances.__len__(), teachingData.__len__(), testData.__len__())
+
+
+    score = kNNAlgorithm(1, teachingData, testData, featuresIds, 'euclidean')
+    print(score)
 
 
 def loadDataFromFile(fileName):
@@ -46,6 +61,53 @@ def kolmogorovTest(instances, quantityOfFeatures):
         featuresRanking.append(ksType(feature, statistic, pValue))
 
     return sorted(featuresRanking, key=ksType.getStatistic, reverse=True)
+
+def crossValidation(instances):
+    teachingData = []
+    testData = []
+    instancesNumber = int(instances.__len__() / 2)
+    usedIndexes = []
+
+    while teachingData.__len__() < instancesNumber:
+        randomIndex = random.randint(0, instances.__len__() - 1)
+
+        if randomIndex not in usedIndexes:
+            teachingData.append(instances[randomIndex])
+            usedIndexes.append(randomIndex)
+
+    for i in range(instances.__len__()):
+        if i not in usedIndexes:
+            testData.append(instances[i])
+
+    return teachingData, testData
+
+def kNNAlgorithm(k, teachingData, testData, features, metric):
+    teachingDataSet, teachingDataLabels = prepareData(teachingData, features)
+    testDataSet, testDataLabels = prepareData(testData, features)
+
+    classifier = KNeighborsClassifier(n_neighbors=k, metric=metric)
+    classifier.fit(teachingDataSet, teachingDataLabels)
+
+    predictions = classifier.predict(testDataSet)
+    score = eval('accuracy_score')(testDataLabels, predictions)
+
+    return score
+
+
+def prepareData(data, features):
+    finalData = []
+    finalDataLabels = []
+
+    for instance in data:
+        featureSet = []
+        finalDataLabels.append(instance.getCancerClass())
+
+        for feature in features:
+            featureSet.append(float(instance.getFeature(feature)))
+
+        finalData.append(featureSet)
+
+    return finalData, finalDataLabels
 
 
 main()
